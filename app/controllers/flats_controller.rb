76f157e8a271c6ed6d0766816617b7d65e75e2a3
Remi Schwartz <schwartz.remi@gmail.com>
2020-11-11 22:22:37 +0100

    index front + show

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,6 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :delete]
+
   def index
     @flats = Flat.all
   end
@@ -7,9 +9,16 @@ class FlatsController < ApplicationController
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    flat = Flat.new(flat_params)
+    if flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,6 +28,16 @@ class FlatsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 
 end
